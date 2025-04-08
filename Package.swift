0e515f117548b84512d0c5332814e761f17e0785
Trevor <trevor.d@beydag.com>
2025-04-08 01:38:11 +0100

    Auto update.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "DJWUIExtensions",
     
     platforms: [
-        .iOS(.v10),
+        .iOS(.v13),
         .macOS(.v10_12),
         .tvOS(.v10),
         .watchOS(.v6)
